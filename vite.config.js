e6b55457e768c52da066c7eea0e597bb1f8dfa73
lichao <lichao@shoplazza.com>
2022-02-20 04:26:24 +0800

    fix(docs): update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import sveltePreprocess from 'svelte-preprocess';
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode })=> {
   return {
-    base: '/',
+    base: '/vite_svelte',
     build: {
       outDir: 'docs',
       minify: false
