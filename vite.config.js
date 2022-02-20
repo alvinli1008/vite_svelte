8d14002196ad887f2c0f52a294b987552c61ce0e
lichao <lichao@shoplazza.com>
2022-02-20 15:23:49 +0800

    fix(partytown): update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,35 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import path from 'path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 import sveltePreprocess from 'svelte-preprocess';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
-export default defineConfig(async ({ mode })=> {
+export default defineConfig(async ({ mode }) => {
   return {
     base: '/vite_svelte',
     build: {
-      outDir: 'docs',
+      outDir: 'docs'
       // minify: false
     },
     plugins: [
       svelte({
         preprocess: sveltePreprocess()
+      }),
+      copy({
+        targets: [
+          {
+            src: [path.resolve('./node_modules/@builder.io/partytown/lib/**/*')],
+            dest: path.join('./', 'public', '~partytown')
+          }
+        ],
+        hook: 'writeBundle' // defaults to 'buildEnd'
       })
-  ],
+    ]
     // resolve: {
     //   alias: {
     //     '~': path.resolve(__dirname, 'src')
     //   }
     // }
-  }
-})
+  };
+});
